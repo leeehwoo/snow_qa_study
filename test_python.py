bc775ad03cc9681bf69c6eb4673e9283e62982fc
leeehwoo <henry.lee@snowcorp.com>
2020-07-08 18:03:39 +0900

    two

---
+++ test_python.py
@@ -0,0 +1,41 @@
+watch = 1000000
+lighter = 300
+print(watch+lighter)
+
+
+a = 'pig'
+b = 'dad'
+print(a+b)
+print(a+' '+b)
+
+
+family = ['Father', 'Mother', 'Son', 'Pet']
+print(len(family))
+
+print(len(family)+watch)
+
+print(family[0])
+
+family.remove('Pet')
+print(family)
+
+for countdown in 5, 4, 3, 2, 1, "Hey!":
+    print(countdown)
+
+cliches = ["At the..", "Having said..", "The fact", "Be that..", "The bottom..", "If you"]
+print(cliches[3])
+
+
+quotes = {
+    "Moe": "A wise guy, huh?",
+    "Larry": "Ow",
+    "Curly": "Nyuk nyuk!",
+}
+stooge = "Curly"
+print(stooge, "says:", quotes[stooge])
+
+a = 951
+print(a)
+print(a-3)
+
+
