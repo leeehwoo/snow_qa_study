684f4358095727171ee9a8c67a959021920970d5
leeehwoo <henry.lee@snowcorp.com>
2020-07-13 17:37:14 +0900

    ex3_2_q1

---
+++ ex3_2_q1.py
@@ -0,0 +1,14 @@
+import requests, json
+
+apiUrl = "http://qa-api.b612kaji.com/v1/filter/overview"
+headers = {'User-Agent': 'iphoneapp.b612cn/9.1.0 (iPhone; U; CPU iOS 13_1_3 like Mac OS X; kr-KR-KR; occ-KR "iPhone X" )'}
+response = requests.get(apiUrl, headers=headers)
+filterOverview = json.loads(response.text)
+
+filters = filterOverview["result"]["filters"]
+filtersCount = 0
+for filtersInfo in filters:
+    filtersName = filters[filtersCount]["name"]
+    filtersId = filters[filtersCount]["id"]
+    filtersCount = filtersCount + 1
+    print('Id : %d, Name : %s' % (filtersId, filtersName))
