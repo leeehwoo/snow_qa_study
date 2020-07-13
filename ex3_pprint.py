e7df0cfba589ce9cf9c25a395d1eaf86ed06b06d
leeehwoo <henry.lee@snowcorp.com>
2020-07-13 17:35:00 +0900

    pprint

---
+++ ex3_pprint.py
@@ -0,0 +1,10 @@
+import requests, json
+import pprint
+
+apiUrl = "http://qa-api.b612kaji.com/v1/filter/overview"
+headers = {
+    'User-Agent': 'iphoneapp.b612cn/9.1.0 (iPhone; U; CPU iOS 13_1_3 like Mac OS X; ko-KR-KR; occ-KR "iPhone X" )'
+}
+response = requests.get(apiUrl, headers=headers)
+filterOverview = json.loads(response.text)
+pprint.pprint(filterOverview)
